cb0660b1afa95b319ba41f22746a601f5a86e6bb
KAZUHIRO JINMA <k-jinma@uhe.ac.jp>
2025-12-10 19:53:28 +0900

    78途中

---
+++ src/lesson78/Ex1.java
@@ -0,0 +1,35 @@
+package lesson78;
+
+import java.io.IOException;
+
+class Ex1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
+
+class Super1 {
+	public void method() throws IOException {
+		
+	}
+}
+
+/* Superクラスのmethod()をオーバーライドする
+ * Subクラスを定義してください。
+ * 例外のオーバーライドは選択肢の中から正しいものを選んでください。
+ * 
+ * 選択肢:
+ * 　a) Exception
+ * 　b) FileNotFoundException
+ * 　c) SQLException
+ * 　d) IOException, SQLException
+ */
+class Sub1 extends Super {
+	
+	@Override
+	
+	
+}
